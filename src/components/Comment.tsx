a2f787cd8e390424412960ffb3e9aed9a195c2cc
Paulo Cavalheiro <palflinho@gmail.com>
2023-02-02 17:30:50 -0300

    Initial commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,54 @@
+import { ThumbsUp } from 'phosphor-react';
+import { Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+    src: string;
+    name: string;
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment ({src, name, content, onDeleteComment}: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+            onDeleteComment(content);
+    }
+
+    function handleLikeComment(){
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return(
+        <div className={styles.Comment}>
+            <Avatar hasBorder={false} src={src}/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>    
+                        <div className={styles.authorAndTime}>
+                            <strong>{name}</strong>
+                            <time dateTime="2023-01-25 17:10:40">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
